e370495e6086ae6a2915504beed1af90b6100a10
simpart <taria.ki4m@gmail.com>
2017-01-15 22:21:50 +0900

    support mofron v0.3.0

--- dist/text.js
+++ dist/text.js
@@ -1,3 +1,4 @@
+require("mofron-event-click");
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -65,83 +66,139 @@
 	mofron.comp.Text = function (_mofron$comp$Base) {
 	    _inherits(_class, _mofron$comp$Base);
 
-	    function _class() {
+	    /**
+	     * initialize font theme
+	     *
+	     * @param prm : (string) text contents
+	     * @param opt : (object) component option
+	     */
+	    function _class(prm, opt) {
 	        _classCallCheck(this, _class);
 
-	        return _possibleConstructorReturn(this, (_class.__proto__ || Object.getPrototypeOf(_class)).apply(this, arguments));
+	        try {
+	            /* font theme */
+	            var _this = _possibleConstructorReturn(this, (_class.__proto__ || Object.getPrototypeOf(_class)).call(this, prm, opt));
+
+	            _this.m_font = null;
+	        } catch (e) {
+	            console.error(e.stack);
+	            throw e;
+	        }
+	        return _this;
 	    }
 
+	    /**
+	     * initialize vdom
+	     * 
+	     * @param vd : (object) vdom object
+	     * @param prm : (string) text contents
+	     */
+
+
 	    _createClass(_class, [{
-	        key: 'initContents',
-	        value: function initContents(vd, prm) {
+	        key: 'initDomConts',
+	        value: function initDomConts(vd, prm) {
 	            try {
+	                this.name('Text');
 	                if ('string' != typeof prm) {
 	                    throw new Error('invalid parameter');
 	                }
 
+	                /* init vdom contents */
 	                var text = new mofron.util.Vdom('div');
-	                text.setText(prm);
+	                text.text(prm);
 	                vd.addChild(text);
+	                this.target = text;
+
+	                /* set style */
 	                this.size(15);
-	                this.setTheme();
+
+	                /* set font theme */
+	                this.m_theme.get(new mofron.util.Font(''));
+	                if (null !== this.m_font) {
+	                    this.setFontTheme(this.m_font);
+	                }
 	            } catch (e) {
 	                console.error(e.stack);
 	                throw e;
 	            }
 	        }
+
+	        /**
+	         * text contents setter/getter
+	         *
+	         * @param val : (string) text contents
+	         * @return (string) text contents
+	         */
+
 	    }, {
 	        key: 'text',
 	        value: function text(val) {
 	            try {
 	                var _val = val === undefined ? null : val;
 	                if (null === _val) {
-	                    return this.getTarget().getText();
+	                    return this.getTarget().text();
 	                }
 	                if ('string' !== typeof _val) {
 	                    throw new Error('invalid parameter');
 	                }
-	                this.getTarget().setText(_val);
+	                this.getTarget().text(_val);
 	            } catch (e) {
 	                console.error(e.stack);
 	                throw e;
 	            }
 	        }
+
+	        /**
+	         * text size setter / getter
+	         *
+	         * @param val : (number) font pixel size (option)
+	         * @return (number) font size
+	         */
+
 	    }, {
 	        key: 'size',
 	        value: function size(val) {
 	            try {
+	                if (undefined === val) {
+	                    /* getter */
+	                    return this.style('font-size');
+	                }
+	                /* setter */
 	                if ('number' != typeof val) {
 	                    throw new Error('invalid parameter');
 	                }
-	                var _val = val === undefined ? null : val;
-	                var txt = this.getTarget();
-	                if (null === _val) {
-	                    return txt.getStyle('font-size');
-	                }
-	                txt.setStyle('font-size', val + 'px');
+	                this.style('font-size', val + 'px');
 	            } catch (e) {
 	                console.error(e.stack);
 	                throw e;
 	            }
 	        }
-	    }, {
-	        key: 'align',
-	        value: function align(tp) {
-	            try {
-	                //var style = new mofron.other.Styles(this, ' .text-conts');
-	                //style.style('text-align', tp);
-	            } catch (e) {
-	                console.error(e.stack);
-	                throw e;
-	            }
-	        }
+
+	        //align (tp) {
+	        //    try {
+	        //        //var style = new mofron.other.Styles(this, ' .text-conts');
+	        //        //style.style('text-align', tp);
+	        //    } catch (e) {
+	        //        console.error(e.stack);
+	        //        throw e;
+	        //    }
+	        //}
+
+	        /**
+	         * set link text
+	         *
+	         * @param url : (string) link url
+	         * @param tab : (bool) new tab flag
+	         */
+
 	    }, {
 	        key: 'setLink',
 	        value: function setLink(url, tab) {
 	            try {
 	                var _tab = tab === undefined ? false : tab;
-	                this.style('cursor', 'pointer');
-	                var click = new mofron.event.Click();
+	                var click = null;
+
 	                if (false === _tab) {
 	                    click = new mofron.event.Click(function () {
 	                        window.location.href = url;
@@ -151,19 +208,29 @@
 	                        window.open(url, '_blank');
 	                    });
 	                }
+
+	                this.style('cursor', 'pointer');
 	                this.addEvent(click);
 	            } catch (e) {
 	                console.error(e.stack);
 	                throw e;
 	            }
 	        }
+
+	        /**
+	         * text color setter/getter
+	         * 
+	         * @param clr : (mofron.util.Color) color object
+	         * @return (string) color (no-parameter)
+	         */
+
 	    }, {
 	        key: 'color',
 	        value: function color(clr) {
 	            try {
 	                var _clr = clr === undefined ? null : clr;
 	                if (null === _clr) {
-	                    return this.getStyleTgt().getStyle('color');
+	                    return this.style('color');
 	                }
 	                if ('object' !== (typeof _clr === 'undefined' ? 'undefined' : _typeof(_clr))) {
 	                    throw new Error('invalid parameter');
@@ -174,39 +241,49 @@
 	                throw e;
 	            }
 	        }
+
+	        /**
+	         * text font setter/getter
+	         * 
+	         * @param fnt : (mofron.util.Font) font object
+	         * @return (string) font
+	         */
+
 	    }, {
 	        key: 'font',
 	        value: function font(fnt) {
 	            try {
 	                var _fnt = fnt === undefined ? null : fnt;
 	                if (null === _fnt) {
-	                    return this.getStyleTgt().getStyle('font-family');
+	                    /* getter */
+	                    return this.style('font-family');
 	                }
+	                /* setter */
 	                if ('object' !== (typeof _fnt === 'undefined' ? 'undefined' : _typeof(_fnt))) {
 	                    throw new Error('invalid parameter');
 	                }
-	                this.style('font-family', _fnt.getFontFamily());
+	                this.style('font-family', _fnt.getStyle());
 	            } catch (e) {
 	                console.error(e.stack);
 	                throw e;
 	            }
 	        }
+
+	        /**
+	         * set font theme to class name
+	         * 
+	         * @param fnt : (object) font object
+	         */
+
 	    }, {
-	        key: 'getTarget',
-	        value: function getTarget() {
+	        key: 'setFontTheme',
+	        value: function setFontTheme(fnt) {
 	            try {
-	                return this.vdom.getChild(0);
-	            } catch (e) {
-	                console.error(e.stack);
-	                throw e;
-	            }
-	        }
-	    }, {
-	        key: 'setTheme',
-	        value: function setTheme(idx) {
-	            try {
-	                var _idx = idx === undefined ? 0 : idx;
-	                this.getTarget().addClname('mofron-theme-font' + _idx);
+	                var _fnt = fnt === undefined ? null : fnt;
+	                if ('object' !== _fnt) {
+	                    throw new Error('invalid parameter');
+	                }
+	                this.getTarget().addClname(fnt.getThemeClass());
 	            } catch (e) {
 	                console.error(e.stack);
 	                throw e;
