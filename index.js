f46518963f6d3f4207c5cf1aeea8d1f49ef9bd88
simpart <taria.ki4m@gmail.com>
2017-03-09 23:06:53 +0900

    refactoring

--- index.js
+++ index.js
@@ -44,32 +44,26 @@ mofron.comp.Text = class extends mofron.Component {
         try {
             /* init vdom contents */
             var conts = null;
-            var link  = this.link();
-            if (null === link[0]) {
+            if (null === this.link()[0]) {
                 conts = new mofron.Dom('div', this);
             } else {
-                conts = new mofron.Dom('a', this);
-                conts.attr('href', link[0]);
-                if (true === link[1]) {
+                conts = new mofron.Dom({
+                            tag       : 'a', 
+                            component : this,
+                            attr      : { 'href' : this.link()[0] }
+                        });
+                if (true === this.link()[1]) {
                     conts.attr('target', '_blank');
                 }
             }
-
             this.vdom().addChild(conts);
             this.target(conts);
             
             /* set text contents */
-            if (null === this.text()) {
-                this.text(prm);
-            } else {
-                this.text(this.text());
-            }
+            this.text((null === this.text()) ? prm : this.text(), true);
             
             /* set font theme */
-            var fnt = this.theme().getFont(0);
-            if (null !== fnt) {
-                this.font(fnt, true);
-            }
+            this.font((null === this.theme().getFont(0)) ? undefined : this.theme().getFont(0), true);
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -80,10 +74,11 @@ mofron.comp.Text = class extends mofron.Component {
      * text contents setter / getter
      *
      * @param val : (string) text contents
+     * @param ini : (boolean) init flag
      * @return (string) text contents
      * @note do not specify parameters, if use as getter
      */
-    text (val) {
+    text (val, ini) {
         try {
             if (undefined === val) {
                 /* getter */
@@ -93,10 +88,12 @@ mofron.comp.Text = class extends mofron.Component {
             if ('string' !== (typeof val)) {
                 throw new Error('invalid parameter');
             }
-            if (null === this.vdom()) {
-                this.m_text = val;
-            } else {
+            var _ini = (undefined === ini) ? false : ini;
+            if ( (true === this.isRendered()) ||
+                 (true === _ini) ) {
                 this.target().text(val);
+            } else {
+                this.m_text = val;
             }
         } catch (e) {
             console.error(e.stack);
@@ -119,17 +116,14 @@ mofron.comp.Text = class extends mofron.Component {
                 return this.style('font-size');
             }
             /* setter */
-            if (null === val) {
-                this.style('font-size', null);
-                return;
-            }
+            var set_val = null;
             if ('number' === typeof val) {
-                this.style('font-size', val + 'px');
-            } else if ('string' === typeof val) {
-                this.style('font-size', val);
-            } else {
+                set_val = val + 'px';
+            } else if ( (null     !== val) &&
+                        ('string' !== typeof val) ) {
                 throw new Error('invalid parameter');
             }
+            this.style('font-size', set_val);
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -169,7 +163,7 @@ mofron.comp.Text = class extends mofron.Component {
     /**
      * text color setter / getter
      * 
-     * @param clr : (object) mofron.util.Color object
+     * @param clr : (mofron.Color) color object
      * @return (string) color
      * @note do not specify parameters, if use as getter
      */
@@ -180,7 +174,7 @@ mofron.comp.Text = class extends mofron.Component {
                 return mofron.func.getColorObj(this.style('color'));
             }
             /* setter */
-            if ('object' !== (typeof clr)) {
+            if (false === mofron.func.isObject(clr, 'Color')) {
                 throw new Error('invalid parameter');
             }
             this.style('color', clr.getStyle());
@@ -208,7 +202,8 @@ mofron.comp.Text = class extends mofron.Component {
             }
             /* setter */
             var _thm = (undefined === thm) ? false : thm;
-            if (('object' !== typeof fnt) || ('boolean' !== typeof _thm)) {
+            if ( (false     === mofron.func.isObject(fnt, 'Font')) ||
+                 ('boolean' !== typeof _thm) ) {
                 throw new Error('invalid parameter');
             }
             if (false === _thm) {
